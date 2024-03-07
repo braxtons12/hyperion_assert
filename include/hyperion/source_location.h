d9c67adf867e8515621c7c3e9483e71d3dcace34
Braxton Salyer <braxtonsalyer@gmail.com>
2024-03-06 22:27:00 -0600

    debug source location column

--- include/hyperion/source_location.h
+++ include/hyperion/source_location.h
@@ -141,6 +141,8 @@ namespace hyperion::_test::source_location {
             constexpr auto current = get_location();
 
             REQUIRE_EQ(current.line(), 135_u32);
+            CHECK_EQ(current.column(), 0_u32);
+            CHECK_EQ(current.column(), 16_u32);
             REQUIRE((current.column() == 16_u32 || current.column() == 0_u32));
             const auto file_name = std::string_view{current.file_name()};
 
