204b2f71fb33ec20eff81aef2c43efbdb2e45e8f
Braxton Salyer <braxtonsalyer@gmail.com>
2024-03-07 16:11:37 -0600

    Fix clang build

--- src/test_main.cpp
+++ src/test_main.cpp
@@ -21,7 +21,11 @@
 #include <hyperion/platform/types.h>
 #include <hyperion/source_location.h>
 
-#include <boost/ut.hpp>
+HYPERION_IGNORE_RESERVED_IDENTIFIERS_WARNING_START;
+HYPERION_IGNORE_UNSAFE_BUFFER_WARNING_START;
+    #include <boost/ut.hpp>
+HYPERION_IGNORE_UNSAFE_BUFFER_WARNING_STOP;
+HYPERION_IGNORE_RESERVED_IDENTIFIERS_WARNING_STOP;
 
 using namespace hyperion;      // NOLINT(google-build-using-namespace)
 
